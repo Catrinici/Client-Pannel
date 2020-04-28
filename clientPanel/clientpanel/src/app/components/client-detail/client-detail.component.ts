2af9e3b6c18582491753f0f826365ec9e56b59a3
Catrinici <catrinici.antonina@gmail.com>
2020-04-28 16:56:11 -0700

    second commit

--- clientPanel/clientpanel/src/app/components/client-detail/client-detail.component.ts
+++ clientPanel/clientpanel/src/app/components/client-detail/client-detail.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+
+import { Client } from '../../models/Client';
 
 @Component({
-  selector: 'app-client-detail',
-  templateUrl: './client-detail.component.html',
-  styleUrls: ['./client-detail.component.css']
+	selector: 'app-client-detail',
+	templateUrl: './client-detail.component.html',
+	styleUrls: ['./client-detail.component.css']
 })
 export class ClientDetailComponent implements OnInit {
+	id: string;
+	client: Client;
+	hasBalance: boolean = false;
+	showBalanceUpdateInput: boolean = false;
 
-  constructor() { }
+	constructor(
+		private clientService: ClientService,
+		private router: Router,
+		private route: ActivatedRoute,
+		private flashMessage: FlashMessagesService
+	) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		//Get id from url
+		this.id = this.route.snapshot.params['id'];
+		// Get client
+		this.clientService.getClient(this.id).subscribe(client => {
+			if (client != null) {
+				if (client.balance > 0) {
+					this.hasBalance = true;
+				}
+			}
+
+			this.client = client
+		})
+	}
 
 }
